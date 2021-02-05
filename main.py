3f550a05c8b54374e6bafe6bf75192372c570c96
Jim Neuendorf <jim.neuendorf@gmx.de>
2021-02-05 23:01:37 +0100

    using while loop so cron doesnt have to be used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
 import os.path
-import sys
+import time
 
 from fritzconnection.lib.fritzstatus import FritzStatus
 from fritzconnection.core.exceptions import FritzConnectionException
@@ -16,15 +16,17 @@ if __name__ == "__main__":
             error_file.write(str(e) + '\n')
 
     write_header = not os.path.isfile(settings.output_file)
-    try:
-        with open(settings.output_file, mode='a+') as output_file:
-            writer = csv.writer(output_file)
-            if write_header:
-                writer.writerow(settings.attrs)
-            writer.writerow([getattr(status, attr) for attr in settings.attrs])
-    except Exception:
-        with open(settings.error_file, mode='a+') as error_file:
-            error_file.write(f'Could not open "{settings.output_file}"\n')
-        raise
+    while True:
+        try:
+            with open(settings.output_file, mode='a+') as output_file:
+                writer = csv.writer(output_file)
+                if write_header:
+                    write_header = False
+                    writer.writerow(settings.attrs)
+                writer.writerow([getattr(status, attr) for attr in settings.attrs])
+            time.sleep(30)
+        except Exception:
+            with open(settings.error_file, mode='a+') as error_file:
+                error_file.write(str(e) + '\n')
 
 
